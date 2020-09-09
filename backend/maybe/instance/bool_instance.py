920b51f5dc9e2919dd4c9e735df19c5612df54f3
半片叶 <34050019+HalfLeaf@users.noreply.github.com>
2020-09-09 20:21:24 +0800

    新增布尔类型代码

--- backend/maybe/instance/bool_instance.py
+++ backend/maybe/instance/bool_instance.py
@@ -8,14 +8,13 @@
 #
 #  Created:     2020.08.11
 #
-#        _       __    _     ___   ____
-#       | |\/|  / /\  \ \_/ | |_) | |_
+#         _       __    _     ___   ____
+#        | |\/|  / /\  \ \_/ | |_) | |_
 #       |_|  | /_/--\  |_|  |_|_) |_|__
 # ----------------------------------------------------------------------------
 '''
 
-from typing import Any
-from models.mongo_client import MongoDatabase
+from maybe.faker_provider import fake
 
 class BoolData():
     def __init__(self):
@@ -24,61 +23,12 @@ class BoolData():
             外部参数:
                 无
             内部参数:
-                :private _effective:有效域值缓存对象
-                :private _invalid:无效域值缓存对象
-                :private _experience:推测域值缓存对象
-        """
-        self.db = MongoDatabase().db
-        self._effective = None
-        self._invalid = None
-        self._experience = None
-
-    def get(self, field="") -> Any:
-        """
-        获取bool类型值
-            外部参数:
-                :param field:指定的参数域
-            返回值:
-        """
-        if field == "invalid":
-            return self._get_invalid_field()
-        elif field == "experience":
-            return self._get_experience_field()
-        else:
-            return self._get_effective_field()
-
-    def _get_effective_field(self) -> bool:
-        """
-        获取Bool类型的有效域
-            外部参数:
                 无
-            返回值:
-                正确的Python Bool类型数据
         """
-        if not self._effective:
-            self._effective = [x.get('value') for x in list(self.db.bool_instance.find({"field":"effective", "status":1}, {"value":1}))]
-        return self._effective
+        pass
 
-    def _get_invalid_field(self) -> bool:
-        """
-        获取Bool类型的无效域
-            外部参数:
-                无
-            返回值:
-                正确的Python Bool类型数据
-        """
-        if not self._invalid:
-            self._invalid = [x.get('value') for x in list(self.db.bool_instance.find({"field":"invalid", "status":1}, {"value":1}))]
-        return self._invalid
+    def parser(self):
+        pass
 
-    def _get_experience_field(self) -> bool:
-        """
-        获取Bool类型的推测域
-            外部参数:
-                无
-            返回值:
-                正确的Python Bool类型数据
-        """
-        if self._experience:
-            self._experience = [x.get('value') for x in list(self.db.bool_instance.find({"field":"experience", "status":1}, {"value":1}))]
-        return self._experience
+    def get(self) -> bool:
+        return fake.pybool()
